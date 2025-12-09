13e917f053c6975ffecad9b695a687239baf98ef
ranajitchandra <ranajitchandradas3@gmail.com>
2025-12-09 19:47:54 +0600

    row_nodeJS_server_with_ts

---
+++ node_js_CURD_typescript_server_M11/src/server.ts
@@ -0,0 +1,36 @@
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
+import { RouteHandler, routes } from "./helpers/RouteHandler";
+import "./routes";
+import findDynamicRoute from "./helpers/dynamicRouteHandler";
+
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    const method = req.method?.toUpperCase() || "";
+    const path = req.url || "";
+
+    const methodMap = routes.get(method);
+    const handler: RouteHandler | undefined = methodMap?.get(path);
+
+    if (handler) {
+      handler(req, res);
+    } else if (findDynamicRoute(method, path)) {
+      const match = findDynamicRoute(method, path);
+      (req as any).params = match?.params;
+      match?.handler(req, res);
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          success: false,
+          message: "Route not found!!!",
+          path,
+        })
+      );
+    }
+  }
+);
+
+server.listen(config.port, () => {
+  console.log(`server is running on port ${config.port}`);
+});
